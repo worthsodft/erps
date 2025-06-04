2dc43ee0c362cf3fb679468058272d7311f361cd
windows1122334 <windows_1122334@126.com>
2025-06-04 10:42:54 +0800

    部署标准版

---
+++ extend/vandles/model/UserMoneyLogModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace vandles\model;
+
+use think\facade\Db;
+use think\helper\Str;
+use vandles\lib\VException;
+
+class UserMoneyLogModel extends BaseSoftDeleteModel {
+
+    protected $name = 'AUserMoneyLog';
+
+}
